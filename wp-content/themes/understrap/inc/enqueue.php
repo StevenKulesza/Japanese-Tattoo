8af8390a69855ec9fc0053a9d3fa67c0dd64dd00
steven <stevenn.kulesza@gmail.com>
2016-12-30 00:02:01 -0500

    rm emojis

--- wp-content/themes/understrap/inc/enqueue.php
+++ wp-content/themes/understrap/inc/enqueue.php
@@ -37,3 +37,7 @@ function understrap_slider(){
 }
 endif;
 
+
+// remove emojis that wordpress added
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
